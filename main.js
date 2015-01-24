ffd24dcff84a8873dbff10229970ae79401ab409
Bob Holben <bholben@gmail.com>
2015-01-24 16:15:30 -0500

    Continue

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 
-var method = 'Encrypted',
+var whichWay = 'Encrypt',
     results = '',
     btn = $('span.btn'),
+    // inputjq = $('#inputText'),
+    // input = document.getElementById('inputText'),
     submit = $('input[type="submit"'),
     wrapper = $('.wrapper');
 
-btn.on('click', function () {
+
+var clearResults = function () {
+  $('.results').remove();
+};
+
+btn.on('click', function (e) {
+  clearResults();
+  // Highlight the clicked button.
   btn.removeClass('active');
   $(this).addClass('active');
+  // Update the whichWay variable
+  whichWay = e.target.textContent;
 });
 
-submit.on('click', function () {
+submit.on('click', function (e) {
+  e.preventDefault();
+  clearResults();
+  results = document.getElementById('inputText').value + '  asdfsdf'
   wrapper.append('' +
     '<div class="results">' +
-      '<h4>' + method + ' text:</h4>' +
+      '<h4>' + whichWay + 'ed text:</h4>' +
       '<p>' + results + '</p>' +
     '</div>')
 });
