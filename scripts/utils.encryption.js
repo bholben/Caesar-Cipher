8887bf33fdacc8afd80caa24f8c1678cd10edd9a
Bob Holben <bholben@gmail.com>
2015-01-24 23:04:30 -0500

    Wrap code in IIFE & objects

--- scripts/utils.encryption.js
+++ scripts/utils.encryption.js
@@ -1,36 +1,47 @@
 
 // Encryption / decryption routine is in this file.
 
-var isLower = function (charCode) {
-  if (charCode >= 97 && charCode <= 122) {
-    return 97;
-  };
+var utils = {
+  isLower: function (charCode) {
+    if (charCode >= 97 && charCode <= 122) {
+      return 97;
+    };
+  },
+
+  isUpper: function (charCode) {
+    if (charCode >= 65 && charCode <= 90) {
+      return 65;
+    };
+  }
 };
 
-var isUpper = function (charCode) {
-  if (charCode >= 65 && charCode <= 90) {
-    return 65;
-  };
+var rotEncryption = {
+
+  char: function (direction, offset, char) {
+    var charCodeOld = char.charCodeAt(0),
+        upper = utils.isUpper(charCodeOld),
+        lower = utils.isLower(charCodeOld);
+    // console.log(charCodeOld, upper, lower);
+    if (upper || lower) {
+      if (direction === 'Decode') { offset = 26 - offset };
+      var charCodeA = upper || lower;
+      console.log(charCodeA);
+      charCodeNew = (charCodeOld - charCodeA + offset) % 26 + charCodeA;
+      return String.fromCharCode(charCodeNew);
+    };
+    return char;
+  },
+
+  text: function (direction, offset, text) {
+    console.log(direction, offset, text)
+    var result = '';
+    for (i in text) {
+      result += this.char(direction, offset, text[i]);
+    };
+    console.log(result);
+    return result;
+  }
+
 };
 
-var rotChar = function (direction, offset, char) {
-  var charCodeOld = char.charCodeAt(0),
-      upper = isUpper(charCodeOld),
-      lower = isLower(charCodeOld);
-  if (upper || lower) {
-    if (direction === 'Decrypt') { offset = 26 - offset };
-    var charCodeA = upper || lower;
-    charCodeNew = (charCodeOld - charCodeA + offset) % 26 + charCodeA;
-    return String.fromCharCode(charCodeNew);
-  };
-  return char;
-};
-
-var rotString = function (direction, offset, str) {
-  var result = '';
-  for (i in str) {
-    result += rotChar(direction, offset, str[i]);
-  };
-  return result;
-};
-
+console.log(rotEncryption.text('Encode', 1, 'abcdef'));
